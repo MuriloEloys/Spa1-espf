4e1970d73f7cd3c869d3b5826469dcc00ffd142d
MuriloEloys <rm552117@gmail.com>
2023-09-04 09:54:56 -0300

    Finalização

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Cabecalho from "./components/cabecalho"
 import Conteudo from "./components/Conteudo"
 import Rodape from "./components/Rodape"
+import viteLogo from "./assets/vite.svg"
 
 export default function App(){
+
+  let viteLogoAlt = "Vite Logo"
 
   return(
     <>
@@ -11,7 +14,7 @@ export default function App(){
 
     <Cabecalho/>
 
-    <Conteudo/>
+    <Conteudo viteLogoProps={viteLogo} viteLogoAlt={viteLogoAlt}/>
 
     <Rodape/>
     </div>
--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,11 +1,17 @@
-export default function Conteudo(){
+import reactLogo from "../assets/react.svg"
+
+export default function Conteudo(props){
+
+let reacLogoAlt = "ReactLogo"
+
     return(
         <>      <section>
         <div>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum repellat incidunt, ut quisquam sapiente explicabo earum amet saepe minus laborum facilis fuga veritatis optio esse eos sed recusandae laudantium necessitatibus.</p>
           <p>Itaque ipsum, accusantium cumque sapiente suscipit voluptatibus amet dolorem animi impedit dicta. Temporibus molestias officia delectus repellendus est, quis hic recusandae porro perspiciatis quidem culpa, itaque tempora reiciendis quasi earum!</p>
           <p>Iusto nam modi natus perferendis asperiores architecto officia rem minus cupiditate molestias quas, voluptatibus praesentium placeat ad assumenda quam totam molestiae porro sunt? Facere amet fugiat, veniam reprehenderit culpa quam?</p>
-          <img src="./assets/react.svg" alt="Logo React" />
+          <img src={reactLogo} alt={reacLogoAlt} />
+          <img src={props.viteLogoProps} alt={props.viteLogoAltProps} />
         </div>
       </section></>
     )
