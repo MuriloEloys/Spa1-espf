41c9a95255127edacde5e9745a1edb52c091b3cd
MuriloEloys <rm552117@fiap.com.br>
2023-10-09 09:48:33 -0300

    Edição de podutos do rpfoessor

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import "./Cabecalho.scss";
 
 export default function Cabecalho() {
+const rotaAtual = useLocation();
+
   return (
     <>
-      <header>
+      <header className="cabecalho">
         <h1>Vite+React coded by - RM552117</h1>
         <nav>
           <ul>
-            <li><Link to="/">Home</Link> </li>
-            <li><Link to="/produtos">Produtos</Link> </li>
+            <li><Link to="/" className={rotaAtual.pathname == "/" ? "active" : ""}>Home</Link> </li>
+            <li><Link to="/produtos" className={rotaAtual.pathname == "/produtos" ? "active" : ""}>Produtos</Link> </li>
             <li><Link to="/editar/produtos/1">Editar Produtos</Link> </li>
             <li><Link to="/excluir">Excluir Produtos</Link></li>
           </ul>
--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,50 +1,126 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
-import { ListaProduto } from '../components/ListaProdutos';
 
 export default function EditarProdutos() {
 
     const {id} = useParams();
 
-    document.title = "Editar Produtos " + id;
+    document.title = "Editar Produtos " + id; 
 
-    const produtoRecuperadoPorId = ListaProduto.filter(item => item.id == parseInt(id))[0];
+    const[produto,setProduto] = useState({});
 
+    //Criar uma estratégia para recuperar o produto da API-JSON com fetch, utilizando GET:
+    useEffect(()=>{
+      
+      fetch(`http://localhost:5000/produtos/${id}`)
+      .then((response)=> response.json())
+      .then((response)=> setProduto(response))
+      .catch(error=> console.log(error));
+
+    },[id]);
+    
 
   return (
     <div>
         <h1>Editar Produtos</h1>
-        <h2>Produto selecinado : {id}</h2>
-        <h2>Poduto nome : {produtoRecuperadoPorId.nome}
-        </h2>
-        <h2>Poduto descrição : {produtoRecuperadoPorId.desc}
-        </h2>
-        <h2>Produto preço : {produtoRecuperadoPorId.preco}
-        </h2>
-        <div>
-          <form >
-            <fieldset>
-              <legend>Produto Selecionado</legend>
-              <div>
-                <label htmlFor="">Nome :</label>
-                <input type="text" name='nome' placeholder='Digite o nome do Produto.'/>
-              </div>
-              
-              <div>
-                <label htmlFor="">Descrição :</label>
-                <input type="text" name='desc' placeholder='Digite a descrição do produto.'/>
-              </div>
-              <div>
-                <label htmlFor="">Preço :</label>
-                <input type="text" name='preco' placeholder='Digite o preço do produto.'/>
-              </div>
-              <div>
-                <button>EDITAR</button>
-              </div>
+          <div>
+            <form>
+              <fieldset>
+                <legend>Produto Selecionado</legend>
+                <div>
+                  <label htmlFor="">Nome:</label>
+                  <input type="text" name="nome" placeholder="Digite o nome do Produto." value={produto.nome} onChange={(e)=> setProduto(e.target.value)}/>
+                </div>
+                <div>
+                  <label htmlFor="">Descrição:</label>
+                  <input type="text" name="desc" placeholder="Digite a descrição do Produto." value={produto.desc} onChange={(e)=> setProduto(e.target.value)}/>
+                </div>
+                <div>
+                  <label htmlFor="">Preço:</label>
+                  <input type="text" name="preco" placeholder="Digite o preço do Produto." value={produto.preco} onChange={(e)=> setProduto(e.target.value)}/>
+                </div>
+                <div>
+                  <button>EDITAR</button>
+                </div>
+              </fieldset>
+            </form>
+          </div>
 
-            </fieldset>
-          </form>
-        </div>
-        
     </div>
   )
 }
+
+
+// MEU CÓDIGO !!!!!! TESTAR  ERRO NOVAMENTE
+// //1ª FORMA DE INPUT COM useState
+// <form>
+// <fieldset>
+//   <legend>Produto Selecionado</legend>
+//   <div>
+//     <label htmlFor="">Nome:</label>
+//     <input type="text" name="nome" placeholder="Digite o nome do Produto." value={produto.nome} onChange={(e)=> setProduto(e.target.value)}/>
+//   </div>
+//   <div>
+//     <label htmlFor="">Descrição:</label>
+//     <input type="text" name="desc" placeholder="Digite a descrição do Produto." value={produto.desc} onChange={(e)=> setProduto(e.target.value)}/>
+//   </div>
+//   <div>
+//     <label htmlFor="">Preço:</label>
+//     <input type="text" name="preco" placeholder="Digite o preço do Produto." value={produto.preco} onChange={(e)=> setProduto(e.target.value)}/>
+//   </div>
+//   <div>
+//     <button>EDITAR</button>
+//   </div>
+// </fieldset>
+// </form>
+
+// import { useParams } from 'react-router-dom'
+// import { useEffect, useState } from 'react';
+
+// export default function EditarProdutos() {
+
+//     const {id} = useParams();
+
+//     document.title = "Editar Produtos " + id;
+
+//     const [produto, setProduto] = useState({})
+
+//     useEffect(()=>{
+
+//       fetch(`http://localhost:5173/produtos${id}`)
+//       .then ((response)=> response.json())
+//       .then ((response)=> setProduto(response))
+//       .catch(error=> console.log(error));
+
+//     },[id]);
+//   return (
+//     <div>
+//         <h1>Editar Produtos</h1>
+//         <div>
+//           <form >
+//             <fieldset>
+//               <legend>Produto Selecionado</legend>
+//               <div>
+//                 <label htmlFor="">Nome :</label>
+//                 <input type="text" name='nome' placeholder='Digite o nome do Produto.' value={produto.nome} onChange={(e)=> setProduto(e.target.value)}/>
+//               </div>
+              
+//               <div>
+//                 <label htmlFor="">Descrição :</label>
+//                 <input type="text" name='desc' placeholder='Digite a descrição do produto.' value={produto.desc}/>
+//               </div>
+//               <div>
+//                 <label htmlFor="">Preço :</label>
+//                 <input type="text" name='preco' placeholder='Digite o preço do produto.' value={produto.preco} />
+//               </div>
+//               <div>
+//                 <button>EDITAR</button>
+//               </div>
+
+//             </fieldset>
+//           </form>
+//         </div>
+        
+//     </div>
+//   )
+// }
