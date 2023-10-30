872dc9a67153a773254b88be55e47f84baafa1e5
MuriloEloys <rm552117@fiap.com.br>
2023-10-30 09:02:19 -0300

    Ajustando o pacote json

---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,18 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>PRI</h1>
+      <h2>Grande Oferta!</h2>
+
+      <div>
+        <figure>
+          <img src="/black-friday-2894130_1920.png" alt="Logo"/>
+          <figcaption></figcaption>
+        </figure>
+      </div>
+
+
+    </div>
+  )
+}
---
+++ src/routes/Login/Login.jsx
@@ -0,0 +1,16 @@
+
+export default function Login() {
+    return (
+      <div>
+          <h1>Login</h1>
+  
+          <div>
+              <form>
+                  
+              </form>
+          </div>
+  
+      </div>
+    )
+  }
+  
